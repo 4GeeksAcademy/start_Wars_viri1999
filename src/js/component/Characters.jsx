1bed672d6a84271627cfa4ced05d8ec467a42912
Viridiana De la cruz <167374369+viridlcrloya141999@users.noreply.github.com>
2024-07-24 05:50:34 +0000

    First commit

---
+++ src/js/component/Characters.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Characters = ()=> {
+    return (
+        <div className="container">
+            <h1 className="text-danger">Characters</h1>
+            <div className="my-carrusel border border-danger">
+                <div className="my-card border border-danger">
+                    <img src="https://placehold.co/400x200" alt="place kitten"/>
+                    <div className="my-card-description">
+                        <h2>Tierra</h2>
+                        <p>Gander</p>
+                        <p>other</p>
+                        <p>other</p>
+                    </div>
+                </div>
+                <div className="my-card border border-danger">
+                    <img src="https://placehold.co/400x200" alt="place kitten"/>
+                    <div className="my-card-description">
+                        <h2>Tierra</h2>
+                        <p>Gander</p>
+                        <p>other</p>
+                        <p>other</p>
+                    </div>
+                </div>
+                <div className="my-card border border-danger">
+                    <img src="https://placehold.co/400x200" alt="place kitten"/>
+                    <div className="my-card-description">
+                        <h2>Tierra</h2>
+                        <p>Gander</p>
+                        <p>other</p>
+                        <p>other</p>
+                    </div>
+                </div>
+                <div className="my-card border border-danger">
+                    <img src="https://placehold.co/400x200" alt="place kitten"/>
+                    <div className="my-card-description">
+                        <h2>Tierra</h2>
+                        <p>Gander</p>
+                        <p>other</p>
+                        <p>other</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Characters
